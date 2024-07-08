51778bc8983b74f7a2ca0a7af1fe15b48d087d81
JiangNightwan <21404752@qq.com>
2024-07-08 10:55:08 +0800

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,71 +1,75 @@
 #!/bin/bash
 
-# 脚本名称：安卓本地环境一键部署
-# 作者：JiangNight
+echo "                                              
+安卓本地环境一键部署
+作者: JiangNight
+"
 
-echo "安卓本地环境一键部署脚本"
-echo "作者: JiangNight"
-
-# 检查网络连接
-if ! ping -c 1 www.baidu.com &> /dev/null; then
-    echo "网络连接不可用，请检查网络设置。"
-    exit 1
+# 添加检测当前IP地址的命令
+ip_address=$(ip addr show | grep inet | grep -v inet6 | grep -v 127.0.0.1 | awk '{print $2}' | cut -d '/' -f 1)
+if [ -n "$ip_address" ]; then
+    echo "当前IP地址: $ip_address"
+else
+    echo "无法获取当前IP地址"
 fi
 
-echo -e "\033[0;31m确保网络状态良好后按回车继续，脚本会自动处理所有提示。\033[0m"
-read -r user_input
+echo -e "\033[0;31m确保网络状态良好后按回车继续，遇到需要选择的选项手动输入y\033[0m\n"
 
-# 定义当前Ubuntu安装目录
-current="/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu"
-
-# 更新和升级pkg
-echo "正在更新pkg..."
+# 更新和升级 pkg
 pkg update && pkg upgrade -y
-if [ $? -ne 0 ]; then
-    echo "pkg更新失败，请检查网络连接或Termux设置。"
+if [ $? -eq 0 ]; then
+    echo -e "\033[0;32mpkg 更新和升级成功。\033[0m"
+else
+    echo -e "\033[0;31mpkg 更新和升级失败。\033[0m"
     exit 1
 fi
 
-# 更新和升级apt
-echo "正在更新apt..."
+# 更新和升级 apt
 apt update && apt upgrade -y
-if [ $? -ne 0 ]; then
-    echo "apt更新失败，请检查网络连接或Ubuntu安装。"
+if [ $? -eq 0 ]; then
+    echo -e "\033[0;32mapt 更新和升级成功。\033[0m"
+else
+    echo -e "\033[0;31mapt 更新和升级失败。\033[0m"
     exit 1
 fi
 
-# 安装proot-distro
-echo "正在安装proot-distro..."
+# 安装 proot-distro
 pkg install proot-distro -y
-if [ $? -ne 0 ]; then
-    echo "proot-distro安装失败。"
+if [ $? -eq 0 ]; then
+    echo -e "\033[0;32mproot-distro 安装成功。\033[0m"
+else
+    echo -e "\033[0;31mproot-distro 安装失败。\033[0m"
     exit 1
 fi
 
-# 安装Ubuntu
-echo "正在安装Ubuntu..."
+# 使用 proot-distro 安装 Ubuntu
 proot-distro install ubuntu
-if [ $? -ne 0 ]; then
-    echo "Ubuntu安装失败。"
+if [ $? -eq 0 ]; then
+    echo -e "\033[0;32mUbuntu 安装成功。\033[0m"
+else
+    echo -e "\033[0;31mUbuntu 安装失败。\033[0m"
     exit 1
 fi
 
-# 添加登录命令到.bashrc
-echo "正在添加登录命令到.bashrc..."
-echo "proot-distro login ubuntu" >> "$HOME/.bashrc"
-if [ $? -ne 0 ]; then
-    echo "添加命令到.bashrc失败。"
+# 添加登录命令到 .bashrc 文件
+echo "proot-distro login ubuntu" >> /data/data/com.termux/files/home/.bashrc
+if [ $? -eq 0 ]; then
+    echo -e "\033[0;32m.bashrc 文件中添加命令成功。\033[0m"
+else
+    echo -e "\033[0;31m添加命令到 .bashrc 文件失败。\033[0m"
     exit 1
 fi
 
-# 执行.bashrc
-echo "正在执行.bashrc..."
-source "$HOME/.bashrc"
-if [ $? -ne 0 ]; then
-    echo "执行.bashrc失败。"
+# 源化 .bashrc 文件
+source /data/data/com.termux/files/home/.bashrc
+if [ $? -eq 0 ]; then
+    echo -e "\033[0;32m.bashrc 文件源化成功。\033[0m"
+else
+    echo -e "\033[0;31m.bashrc 文件源化失败。\033[0m"
     exit 1
 fi
 
-echo "环境部署完成。Termux将在5秒后退出。"
+# 最终消息和退出
+echo -e "\033[0;32m环境设置完成。Termux将在5秒后退出。\033[0m"
 sleep 5
-exit 0
+exit
